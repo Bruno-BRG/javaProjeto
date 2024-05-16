4606964e51a4908b4800d6beb816881df4e99475
Bruno Guimarães <brg.brunorocha@gmail.com>
2024-05-15 22:31:24 -0300

    somplificando

--- main.java
+++ main.java
@@ -6,12 +6,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int option = 0;
-		while (option != 5) {
-			System.out.println("1: add a new client");
-			System.out.println("2: add a new buyer");
-			System.out.println("3: see list of available items");
-			System.out.println("4: Give review in a product");
-			System.out.println("5: exit");
+		while (option != 4) {
+			System.out.println("1: Make a new account");
+			System.out.println("2: see list of available items");
+			System.out.println("3: Give review in a product");
+			System.out.println("4: exit");
 			System.out.println("Enter an option: ");
 			option = scanner.nextInt();
 			switch (option) {
@@ -30,23 +29,10 @@ public class Main {
 					break;
 
 				case 2:
-					System.out.println("Enter the name of the buyer: ");
-					String nameBuyer = scanner.next();
-					System.out.println("Enter the email of the buyer: ");
-					String emailBuyer = scanner.next();
-					System.out.println("Enter the zip code of the buyer: ");
-					String zipBuyer = scanner.next();
-					System.out.println("Enter the password of the buyer: ");
-					String passwordBuyer = scanner.next();
-					System.out.println("Enter the username of the buyer: ");
-					String usernameBuyer = scanner.next();
-					Buyer buyer = new Buyer(nameBuyer, emailBuyer, zipBuyer, passwordBuyer, usernameBuyer);
 					break;
 				case 3:
 					break;
 				case 4:
-					break;
-				case 5:
 					break;
 				default:
 					System.out.println("Invalid option");
