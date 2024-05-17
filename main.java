3dd08d8d48814ece80f6e774f9c162cd0098c829
Bruno Guimarães <brg.brunorocha@gmail.com>
2024-05-16 22:33:16 -0300

    souGenial

--- main.java
+++ main.java
@@ -29,15 +29,19 @@ public class Main {
 					break;
 
 				case 2:
-					System.out.println("Enter the name of the product: ");
-					String productName = scanner.next();
-					System.out.println("Enter the price of the product: ");
-					String price = scanner.next();
-					System.out.println("Enter the quantity of the product: ");
-					String quantity = scanner.next();
-					System.out.println("Enter the description of the product: ");
-					String description = scanner.next();
-					Item item = new Item(productName, price, quantity, description);
+					// i want to print all the item in the list here
+					itemList item = new itemList(10);
+					item.addProduct(new Item("item1", "description1", "price1", "quantity1"));
+					item.addProduct(new Item("item2", "description2", "price2", "quantity2"));
+					item.addProduct(new Item("item3", "description3", "price3", "quantity3"));
+					item.addProduct(new Item("item4", "description4", "price4", "quantity4"));
+					item.addProduct(new Item("item5", "description5", "price5", "quantity5"));
+					item.addProduct(new Item("item6", "description6", "price6", "quantity6"));
+					item.addProduct(new Item("item7", "description7", "price7", "quantity7"));
+					item.addProduct(new Item("item8", "description8", "price8", "quantity8"));
+					item.addProduct(new Item("item9", "description9", "price9", "quantity9"));
+					item.addProduct(new Item("item10", "description10", "price10", "quantity10"));
+					item.showItem();
 					
 					break;
 				case 3:
