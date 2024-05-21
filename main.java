6cf0b746769ae092ec1d633f8afc275cc2f61c1d
Bruno Guimarães <brg.brunorocha@gmail.com>
2024-05-20 22:10:16 -0300

    ReveiwQueueAdded

--- main.java
+++ main.java
@@ -12,13 +12,19 @@ public class Main {
 		// inicializando as variaveis
 		Scanner scanner = new Scanner(System.in);
 		int option = 0;
-		purchaseTree purchaseTree = new purchaseTree("root");
+		
 		String name = null;
 		String email = null;
 		String zip = null;
 		String password = null;
 		String username = null;
 		String itemID = null;
+		String itemIDReview = null;
+		int rating = 0;
+		String review = null;
+
+		purchaseTree purchaseTree = new purchaseTree("root");
+		reviewQueue reviewQueue = new reviewQueue();	
 		ClientList ClientList = new ClientList();
 		itemList item = new itemList(100);
 		item.addProduct("item1", "description1", "price1", "quantity1", "itemID1");
@@ -27,7 +33,7 @@ public class Main {
 		item.addProduct("item4", "description4", "price4", "quantity4", "itemID4");
 		item.addProduct("item5", "description5", "price5", "quantity5", "itemID5");
 
-		// switch case para mostrar na tela as oprcoes que o usuario tem
+				// switch case para mostrar na tela as oprcoes que o usuario tem
 		while (option != 6) {
 			System.out.println("1: Make a new account");
 			System.out.println("2: see list of available items");
@@ -83,6 +89,15 @@ public class Main {
 
 				// dar review para um item comprado recentemente
 				case 5:
+					System.out.println("Enter the itemID of the item you want to review: ");
+					itemIDReview = scanner.next();
+					System.out.println("Enter the rating from 1 to 5");			
+					rating = scanner.nextInt();
+					System.out.println("Enter the review: ");
+					review = scanner.next();
+					System.out.println("Review added successfully");
+					reviewQueue.addReview(itemIDReview, rating, review);	
+					reviewQueue.showReviews();
 					break;
 
 				// sair do programa
